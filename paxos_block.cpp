66f74ed4ba61768ab66daf47b28ca0038dfbb4d9
Mario Infante <marioinfante@umail.ucsb.edu>
2018-06-04 18:40:06 -0700

    added message handler. Cleaned and Commented code.

--- paxos_block.cpp
+++ paxos_block.cpp
@@ -3,34 +3,99 @@
 /* MESSAGE HANDLER THREAD:
  * Handle any message received from UDP/TCP servers and respond accordingly
  */
-void message_handler(char* message){
-  //proposal
-  if(message[0] == 'p'){
-    printf("proposal\n");
+void* message_handler(void* arg){
+  char* message = (char*) arg;
+  std::string msg_s(message);
+  std::istringstream iss(msg_s);
+
+  /*
+   * Received: "Prepare <ballot> <their.id>"
+   * On prepare:
+   *   
+   */
+  if(message[0] == 'P'){
+    std::string p,b,i;
+    iss >> std::skipws >> p;
+    iss >> std::skipws >> b;
+    iss >> std::skipws >> i;
+    int their_ballotnum = stoi(b);
+    int their_id = stoi(i);
+    std::cout << "Received from node " << std::to_string(their_id)
+	      << ": " << msg_s << std::endl;
+
+    // ballot check
+    if(their_ballotnum >= ballot_num[0]){
+
+      if(their_ballotnum == ballot_num[0] && their_id < ballot_num[1]){
+	//do nothing, I have higher ballotnum
+      }
+
+      // they have higher ballotnum
+      else{
+	// update ballotnum
+	ballot_num[0] = their_ballotnum;	
+
+	// build reply message
+	std::string b_msg = "Reply " + std::to_string(their_ballotnum) + " "
+	  + std::to_string(their_id) + " " + std::to_string(accept_num[0])
+	  + " " + std::to_string(accept_num[1]) + " " + std::to_string(accept_val)
+	  + " " + std::to_string(id);
+
+	// send
+	sendto(socks[their_id], (const char*) b_msg.c_str(), strlen(b_msg.c_str())
+	       , MSG_CONFIRM, (const struct sockaddr*) &servaddrs[their_id],
+	       sizeof(servaddrs[their_id]));
+      }
+    }
   }
-  //ack/reply
-  else if(message[0] == 'r'){
-    printf("ack\n");
+
+  //"ack"
+  //Reply <ballotnum> <id> <their.acceptnum> <their.acceptid> <their.acceptval> <their.id>
+  else if(message[0] == 'R'){
+    // update relevant global ack counter
+    std::string r, b, i, tan, tai,tav, tid;
+    iss >> std::skipws >> r;
+    iss >> std::skipws >> b;
+    iss >> std::skipws >> i;
+    iss >> std::skipws >> tan;
+    iss >> std::skipws >> tai;
+    iss >> std::skipws >> tav;
+    iss >> std::skipws >> tid;
+    int ballot = stoi(b);
+    int ballot_id = stoi(i);
+    int their_acceptnum = stoi(tan);
+    int their_acceptid = stoi(tai);
+    int their_acceptval = stoi(tav);
+    int their_id = stoi(tid);
+    std::cout << "Received from node " << std::to_string(their_id) << ": " << msg_s << std::endl;
   }
-  //acceot
-  else if(message[0] == 'a'){
-    printf("accept\n");
+  
+  //Accept <ballotnum> <id> <acceptval> <their.id>
+  else if(message[0] == 'A' || message[0]){
+    // update relevant global accept counter
   }
   //nack
-  else if(message[0] == 'n'){
+  else if(message[0] == 'N'){
     printf("nack\n");
+
+    // usage tbd...
   }
   //poll for update
-  else if(message[0] == 'b'){
+  else if(message[0] == 'B'){
     printf("poll for update\n");
+
+    // send replicated log
   }
   //update
-  else if(message[0] == 'u'){
+  else if(message[0] == 'U'){
     printf("update blockchain\n");
+
+    // update local replicated log
   }
   else{
     printf("unknown message received: %s\n", message);
   }
+  pthread_exit(NULL);
 }
 
 /* UDP SERVER THREAD:
@@ -40,11 +105,11 @@ void message_handler(char* message){
  */
 void* udp_server(void*){
   printf("Initializing UDP Server...\n");
-  int sockf;
+  int sockfd;
   char buffer[1024];
   struct sockaddr_in servaddr, cliaddr;
 
-  if ( (sockf = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+  if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
     std::cerr << "socket creation failed" << std::endl;
     std::exit(1);
   }
@@ -56,7 +121,7 @@ void* udp_server(void*){
   servaddr.sin_addr.s_addr = INADDR_ANY;
   servaddr.sin_port = htons(ports[id]);
 
-  if(bind(sockf, (const struct sockaddr *)&servaddr,sizeof(servaddr)) < 0 ){
+  if(bind(sockfd, (const struct sockaddr *)&servaddr,sizeof(servaddr)) < 0 ){
     std::cerr << "bind failed" << std::endl;
     std::exit(1);
   }
@@ -65,12 +130,21 @@ void* udp_server(void*){
   socklen_t len;
   while(true){
     memset(&buffer, 0, 1024);
-    n = recvfrom(sockf, (char*) buffer, 1024, MSG_WAITALL,
+    n = recvfrom(sockfd, (char*) buffer, 1024, MSG_WAITALL,
 		 (struct sockaddr *) &cliaddr, &len);
+    if(n < 0){
+      std::cout << "recvfrom returned: " << n << std::endl;
+      std::exit(1);
+    }
     buffer[n] = '\0';
-    printf("Message Received: %s", buffer);
+    
+    // add message to heap before passing over to thread
+    char* arg = (char*) malloc(strlen(buffer));
+    strcpy(arg, buffer);
+    thread_i++;
+    pthread_create(&threads[thread_i], NULL, message_handler, (void*) arg);
 
-    message_handler(buffer);
+    usleep(1000000);
   }
   pthread_exit(NULL);
 }
@@ -116,44 +190,103 @@ void serversetup(){
   }
 }
 
+/*
+ * Broadcast an input to ALL servers
+ */
+void broadcast(char* input){
+  printf("Broadcast: %s\n", input);
+  for(int i = 0; i < 5; i++){
+    sendto(socks[i], (const char*) input, strlen(input), MSG_CONFIRM,
+	   (const struct sockaddr*) &servaddrs[i], sizeof(servaddrs[i]));
+  }
+}
+
 int main(int argc, char* argv[]){
   // ./program <id>
   if(argc != 2){
-    std::cerr << "usage err" << std::endl;
+    std::cerr << "Usage: ./paxos_block <id>" << std::endl;
   }
+
+  // update id accordingly
   id = atoi(argv[1]);
 
-  //Get and store IP/Ports of other servers
+  // Get and store network variables
   serversetup();
 
-  //Start threads
-  int num_threads = 100;
-  pthread_t* threads = (pthread_t*) malloc(num_threads);
+  // Start threads
+  threads = (pthread_t*) malloc(num_threads);
   memset(threads,0,num_threads);
-  int thread_i = 0;
-
   thread_i++;
 
+  // UDP Server threads, constantly listening and handling incoming messages
   pthread_create(&threads[thread_i], NULL, udp_server, NULL);
 
+  // Wait 1 second to let udp_server to start up
   usleep(1000000);
   
-  //Start commands
-   std::string input;
+  // Start allowing for command input 
+  std::string input;
   printf("Input command: \n");
   std::getline(std::cin, input);
-
   while(input.compare("exit") != 0){
-    //propose <int>
+    /*
+     * On propose, broadcast "propose ballot_num++ myId"
+     */
     if((input.at(0) == 'P' || input.at(0) == 'p')){
-      //propose(input);
+      ballot_num[0]++;
+      ballot_num[1] = id;
+      std::string prep = "Prepare " + std::to_string(ballot_num[0])
+	+ " " + std::to_string(ballot_num[1]);
+      broadcast((char*)prep.c_str());
     }
     else{
       printf("Command error.\n");
     }
+
+    //Short wait for command thread to run properly
+    usleep(1000000);
+    
     printf("Input command: \n");
     std::getline(std::cin, input);
   }
   return 0;
 
 }
+
+
+/* NOT POSSIBLE? atttempts: boost::timeout, std::future
+ *
+ * Constantly check if a majority of acks or accepts has been achieved and return true.
+ * At timeout, the function will be terminated regardless of majority achieved and assume
+ * false.
+ *
+bool majority_counter(char* arg){
+  std::string arg_s(arg);
+  std::istringstream iss(arg_s);
+  
+  // Counting Acks
+  if(arg[0] == 'R'){
+    std::string x_s, y_s;
+    iss >> std::skipws >> x_s;
+    iss >> std::skipws >> y_s;
+    int x = stoi(x_s);
+    int y = stoi(y_s);
+    while(ack[x][y] < 3){
+      usleep(1000000);
+    }
+  }
+
+  // Counting Accepts
+  else{
+    std::string x_s, y_s;
+    iss >> std::skipws >> x_s;
+    iss >> std::skipws >> y_s;
+    int x = stoi(x_s);
+    int y = stoi(y_s);
+    while(accepts[x][y] < 3){
+      usleep(1000000)
+    }
+  }
+  return true;
+}
+*/
--- paxos_block.h
+++ paxos_block.h
@@ -5,6 +5,7 @@
 #include <string> //std::string
 #include <string.h> //strcpy, strlen
 #include <cstring> //for memset
+#include <sstream> //iss
 
 #include <fstream> //file input
 
@@ -21,15 +22,38 @@
 #include <sys/types.h> //special types pther_t
 #include <sys/socket.h> //sockets
 
+#include <vector> //keep track of majorities
 
+// PAXOS Variables
 int id;
+int ballot_num[2] = {0,0};
+int accept_num[2] = {0,0};
+int accept_val = 0;
+int prop_val = 0;
+std::vector<int> log; //log.push_back
 
+// Global Majority Variables
+int ack[100][5];
+int accepts[100][5];
+
+// Network Variables
 std::string servers[5];
 int ports[5];
-
 struct sockaddr_in servaddrs[5];
 int socks[5];
 
+// Threading Variables (for anyone to start a thread)
+int num_threads = 100;
+pthread_t* threads;
+int thread_i = 0;
+
+
+// Functions
+//void* command_handler(void*); // char*
+void* message_handler(void*); // char*
+void* udp_server(void*);
+void serversetup();
+void broadcast(char*);
 
 
 #endif
